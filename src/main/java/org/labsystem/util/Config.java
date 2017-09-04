d330608347d2b5d769c7fe0239a236f2ee4babcd
jiean001 <421511240@qq.com>
2017-09-05 00:00:49 +0800

    luxiongbo add config file
    Signed-off-by: jiean001 <421511240@qq.com>

---
+++ src/main/java/org/labsystem/util/Config.java
@@ -0,0 +1,9 @@
+package org.labsystem.util;
+
+public class Config {
+	public static final String LANGUAGE = "Lauguage";
+	//true:  Chinese
+	//false: English
+	public static final boolean DEFAULTLANGUAGE = true;
+
+}

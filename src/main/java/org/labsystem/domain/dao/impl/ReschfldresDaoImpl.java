7f85d87f3bf70a33e5f9d3920501bd2e22ca4e9d
jiean001 <421511240@qq.com>
2017-09-05 15:01:29 +0800

    luxiongbo add research field view
    Signed-off-by: jiean001 <421511240@qq.com>

--- src/main/java/org/labsystem/domain/dao/impl/ReschfldresDaoImpl.java
+++ src/main/java/org/labsystem/domain/dao/impl/ReschfldresDaoImpl.java
@@ -1,13 +1,22 @@
 package org.labsystem.domain.dao.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.labsystem.domain.dao.iface.ReschfldresDao;
+import org.labsystem.domain.dao.iface.ResearchDao;
+import org.labsystem.domain.view.ResearchDetailsView;
 import org.labsystem.entity.Reschfldres;
+import org.labsystem.entity.Research;
+import org.labsystem.entity.Researchfield;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository("ReschfldresDao")
 public class ReschfldresDaoImpl extends GenericDaoImpl<Reschfldres, Integer> implements ReschfldresDao {
+	@Autowired
+	private ResearchDao researchDao;
+
 	@Override
 	public List<Reschfldres> findAll() {
 		// TODO cache
@@ -19,6 +28,35 @@ public class ReschfldresDaoImpl extends GenericDaoImpl<Reschfldres, Integer> imp
 	@Override
 	public Reschfldres get(int id) {
 		return this.getSession().get(Reschfldres.class, id);
+	}
+
+	//通过领域获得子领域
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Research> getResearchsByReschfld(Researchfield entity) {
+		//先获得Reschfldres List
+		String sql_str = "from Reschfldres reschfldres where reschfldres.researchfieldId = " + entity.getResearchfieldId();
+		List<Reschfldres> reschfldres = this.getSession().createQuery(sql_str).list();
+		List<Research> Researchs = new ArrayList<>();
+		//遍历Reschfldres List获得Research List
+		for(Reschfldres rfr: reschfldres) {
+			Research res = researchDao.get(rfr.getResearchId());
+			Researchs.add(res);
+		}
+		return Researchs;
+	}
+
+	@Override
+	public List<ResearchDetailsView> getResearchDetailsViewByResearchfield(Researchfield entity, boolean isChinese) {
+		//获得子领域
+		List<Research> Researchs = this.getResearchsByReschfld(entity);
+		List<ResearchDetailsView> detailsViews = new ArrayList<>();
+		//遍历子领域以便产生领域的详细视图
+		for(Research res: Researchs) {
+			ResearchDetailsView tmpView = new ResearchDetailsView(res, isChinese);
+			detailsViews.add(tmpView);
+		}
+		return detailsViews;
 	}
 
 }
--- src/main/java/org/labsystem/domain/service/iface/ReschfldresService.java
+++ src/main/java/org/labsystem/domain/service/iface/ReschfldresService.java
@@ -2,9 +2,12 @@ package org.labsystem.domain.service.iface;
 
 import java.util.List;
 
-import org.labsystem.entity.Reschfldres;
+import org.labsystem.domain.view.ResearchDetailsView;
 import org.labsystem.entity.Research;
+import org.labsystem.entity.Researchfield;
 
 public interface ReschfldresService {
-	List<Research> getResearchByResearchField(Reschfldres entity);
+	List<Research> getResearchByResearchField(Researchfield entity);
+
+	List<ResearchDetailsView> getResearchDetailsViewByResearchfield(Researchfield entity, boolean isChinese);
 }

c714ad1e7894fdaac54a630fb926d8929ead7b69
jiean001 <421511240@qq.com>
2017-09-07 00:53:32 +0800

    luxiongbo update page
    Signed-off-by: jiean001 <421511240@qq.com>

--- src/main/java/org/labsystem/web/action/IndexAction.java
+++ src/main/java/org/labsystem/web/action/IndexAction.java
@@ -2,33 +2,77 @@ package org.labsystem.web.action;
 
 import java.util.List;
 
-import org.apache.log4j.Logger;
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Result;
 import org.apache.struts2.convention.annotation.Results;
+import org.labsystem.domain.service.iface.FacultyService;
+import org.labsystem.domain.service.iface.LaboratoryInfoService;
+import org.labsystem.domain.service.iface.LatestNewsService;
 import org.labsystem.domain.service.iface.ResearchService;
+import org.labsystem.web.view.LaboratoryInfoView;
+import org.labsystem.web.view.NewsView;
 import org.labsystem.web.view.ResearchFieldView;
+import org.labsystem.web.view.TeacherSimpleView;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.alibaba.fastjson.JSON;
-import com.opensymphony.xwork2.ActionContext;
 
-
-@Results({ @Result(name = "index", location = "/index.jsp"),
+@Results({ @Result(name = "index", location = "/index/index.jsp"),
 })
 public class IndexAction extends BaseAction {
-	private static final Logger LOGGER = Logger.getLogger(IndexAction.class);
-
 	@Autowired
 	private ResearchService researchService;
+	@Autowired
+	private LaboratoryInfoService laboratoryInfoService;
+	@Autowired
+	private FacultyService facultyService;
+	@Autowired
+	private LatestNewsService latestNewsService;
+
+
+	private List<ResearchFieldView> researchFieldViews;
+	private LaboratoryInfoView labInfoView;
+	private List<TeacherSimpleView> teacherSimpleViews;
+	private List<NewsView> newsViews;
 
 	@Action("index")
 	public String index() {
-		List<ResearchFieldView> list = researchService.getAllResearchFieldView(getLanguage());
-		ActionContext.getContext().getValueStack().set("researchFieldList", list);
-		//this.getServletRequest().setAttribute("researchFieldList", list);
-		//session.setAttribute("researchFieldList", list);
-		LOGGER.error("HAHA" + JSON.toJSON(list));
+		setLanguage(!getLanguage());
+		this.labInfoView = laboratoryInfoService.getLaboratoryInfoView(getLanguage());
+		this.researchFieldViews = researchService.getAllResearchFieldView(getLanguage());
+		this.teacherSimpleViews = facultyService.getAllTeacherSimpeView(getLanguage());
+		this.newsViews = latestNewsService.getAllNewsViewsByTimeDesc(getLanguage());
 		return "index";
+	}
+
+	public List<NewsView> getNewsViews() {
+		return newsViews;
+	}
+
+	public void setNewsViews(List<NewsView> newsViews) {
+		this.newsViews = newsViews;
+	}
+
+	public List<TeacherSimpleView> getTeacherSimpleViews() {
+		return teacherSimpleViews;
+	}
+
+	public void setTeacherSimpleViews(List<TeacherSimpleView> teacherSimpleViews) {
+		this.teacherSimpleViews = teacherSimpleViews;
+	}
+
+	public List<ResearchFieldView> getResearchFieldViews() {
+		return researchFieldViews;
+	}
+
+	public void setResearchFieldViews(List<ResearchFieldView> researchFieldViews) {
+		this.researchFieldViews = researchFieldViews;
+	}
+
+	public LaboratoryInfoView getLabInfoView() {
+		return labInfoView;
+	}
+
+	public void setLabInfoView(LaboratoryInfoView labInfoView) {
+		this.labInfoView = labInfoView;
 	}
 }
--- src/main/webapp/index/index.jsp
+++ src/main/webapp/index/index.jsp
@@ -1,31 +1,55 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
-<%@ taglib prefix="s" uri="/struts-tags" %>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="s" uri="/struts-tags"%>
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>${company.name} - 首页</title>
-		<link type="text/css" rel="stylesheet" href="index/css/css.css"/>
-		
-	</head>
+<head>
+<title>${labInfoView.laboratoryName}- 首页</title>
+<link type="text/css" rel="stylesheet" href="index/css/css.css" />
 
-	<body>
-    <div id="all">
-	<div id="left">
-	     <div id="leftbutton1">1234</div>
+</head>
+
+<body>
+	<div id="all">
+		<div id="left">
+			<s:iterator value="researchFieldViews" status="li">
+				<div id="leftbutton1">
+					<s:property value="researchFieldName" />
+				</div>
+				<p></p>
+			</s:iterator>
+
+		</div>
+		<div id=right>
+			<div>${labInfoView.laboratoryTitle}</div>
+			<hr />
+			<div id="body">老师介绍</div>
+			<div>
+				<s:iterator value="teacherSimpleViews" status="li">
+					<div id="leftbutton1">
+						<s:property value="teacherName" />
+						<s:property value="profsnTitle" />
+					</div>
+					<p></p>
+				</s:iterator>
+			</div>
+			<hr />
+			<div id="body">最新消息</div>
+			<div>
+				<s:iterator value="newsViews" status="li">
+					<div id="leftbutton1">
+						<s:property value="newsTitle" />
+						<s:property value="newsTime" />
+					</div>
+					<p></p>
+				</s:iterator>
+			</div>
+			<hr />
+			<div id="body">实验室成员</div>
+			<hr />
+		</div>
+		<hr />
 	</div>
-	<div id=right> 
-	     <div>实验室</div>
-	     <hr />
-	     <div id="body">老师介绍</div>
-	          <div></div> 
-	     <hr />
-	     <div id="body">最新消息</div>
-	     <hr />
-	     <div id="body">实验室成员</div>
-	     <hr />
-	</div>
-	<hr />
-	</div>
-	<div id="footer" style="float:center;">联系我们</div>
-	</body>
+	<div id="footer" style="float: center;">联系我们</div>
+</body>
 </html>

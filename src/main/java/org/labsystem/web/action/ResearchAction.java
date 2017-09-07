e1f441256c5259b36bcca6584ed6b0e6a0a9b830
orangezhangc <1484143738@qq.com>
2017-09-07 18:18:06 +0800

    luxiongbo add
    Signed-off-by: orangezhangc <1484143738@qq.com>

---
+++ src/main/java/org/labsystem/web/action/ResearchAction.java
@@ -0,0 +1,60 @@
+package org.labsystem.web.action;
+
+
+import java.util.List;
+
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Result;
+import org.apache.struts2.convention.annotation.Results;
+import org.labsystem.domain.entity.Researchfield;
+import org.labsystem.domain.service.iface.ResearchService;
+import org.labsystem.web.view.ResearchDetailsView;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Results({@Result(name = "research", location = "/index/research.jsp"), })
+public class ResearchAction extends BaseAction {
+	@Autowired
+	private ResearchService researchService;
+	
+	private int reschfdID;
+	private Researchfield researchField;
+	//子领域的详细试图列表
+	private List<ResearchDetailsView> researchDetailsViews;
+	
+	
+	@Action("people")
+	public String people() {
+        this.researchField = researchService.getResearchfield(reschfdID);
+        this.researchDetailsViews = researchService.getResearchDetailsViewByResearchField(researchField, getLanguage());
+		return "people";
+	}
+	
+	public int getReschfdID() {
+		return reschfdID;
+	}
+
+
+	public void setReschfdID(int reschfdID) {
+		this.reschfdID = reschfdID;
+	}
+
+
+	public Researchfield getResearchField() {
+		return researchField;
+	}
+
+
+	public void setResearchField(Researchfield researchField) {
+		this.researchField = researchField;
+	}
+
+
+	public List<ResearchDetailsView> getResearchDetailsViews() {
+		return researchDetailsViews;
+	}
+
+
+	public void setResearchDetailsViews(List<ResearchDetailsView> researchDetailsViews) {
+		this.researchDetailsViews = researchDetailsViews;
+	}
+}
